dfa1f96676c24074026aed2f4dcbfdbb92ad1723
Dominick Burns <dominickb613@gmail.com>
2018-10-04 18:55:47 -0400

    Add Gaussian Blur, Erode, and Dilate processing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
@@ -1,11 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import org.corningrobotics.enderbots.endercv.OpenCVPipeline;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -50,6 +46,10 @@ public class ExampleBlueVision extends OpenCVPipeline {
     // we declare the Mats up here and reuse them. This is easier on the garbage collector.
     private Mat hsv = new Mat();
     private Mat thresholded = new Mat();
+    private Mat blurOutput = new Mat();
+    private Mat cvErodeOutput = new Mat();
+    private Mat cvDilateOutput = new Mat();
+
 
     // this is just here so we can expose it later thru getContours.
     private List<MatOfPoint> contours = new ArrayList<>();
@@ -69,15 +69,35 @@ public class ExampleBlueVision extends OpenCVPipeline {
         // Then, we threshold our hsv image so that we get a black/white binary image where white
         // is the blues listed in the specified range of values
         // you can use a program like WPILib GRIP to find these values, or just play around.
-        Scalar lower_white = new Scalar(0, 0, 150);
-        Scalar upper_white = new Scalar(255, 65, 255);
-
+        Scalar lower_white = new Scalar(0, 0, 80);
+        Scalar upper_white = new Scalar(255, 45, 255);
 
         Core.inRange(hsv, lower_white, upper_white, thresholded);
 
-        // we blur the thresholded image to remove noise
-        // there are other types of blur like box blur or gaussian which can be explored.
-        Imgproc.blur(thresholded, thresholded, new Size(3, 3));
+        // Step Blur0:
+        Mat blurInput = thresholded;
+        BlurType blurType = BlurType.get("Gaussian Blur");
+        double blurRadius = 2.0;
+        blur(blurInput, blurType, blurRadius, blurOutput);
+
+        // Step CV_erode0:
+        Mat cvErodeSrc = blurOutput;
+        Mat cvErodeKernel = new Mat();
+        Point cvErodeAnchor = new Point(-1, -1);
+        double cvErodeIterations = 2.0;
+        int cvErodeBordertype = Core.BORDER_CONSTANT;
+        Scalar cvErodeBordervalue = new Scalar(-1);
+        cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, cvErodeOutput);
+
+        // Step CV_dilate0:
+        Mat cvDilateSrc = cvErodeOutput;
+        Mat cvDilateKernel = new Mat();
+        Point cvDilateAnchor = new Point(-1, -1);
+        double cvDilateIterations = 2.0;
+        int cvDilateBordertype = Core.BORDER_CONSTANT;
+        Scalar cvDilateBordervalue = new Scalar(-1);
+        cvDilate(cvDilateSrc, cvDilateKernel, cvDilateAnchor, cvDilateIterations, cvDilateBordertype, cvDilateBordervalue, cvDilateOutput);
+
 
         // create a list to hold our contours.
         // Conceptually, there is going to be a single contour for the outline of every blue object
@@ -85,7 +105,7 @@ public class ExampleBlueVision extends OpenCVPipeline {
         // the Imgproc module has many functions to analyze individual contours by their area, avg position, etc.
         contours = new ArrayList<>();
         // this function fills our contours variable with the outlines of blue objects we found
-        Imgproc.findContours(thresholded, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(cvDilateOutput(), contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
         // Then we display our nice little binary threshold on screen
         if (showContours) {
             // this draws the outlines of the blue contours over our original image.
@@ -95,4 +115,153 @@ public class ExampleBlueVision extends OpenCVPipeline {
 
         return rgba; // display the image seen by the camera
     }
+    public Mat blurOutput() {
+        return blurOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_erode.
+     * @return Mat output from CV_erode.
+     */
+    public Mat cvErodeOutput() {
+        return cvErodeOutput;
+    }
+
+    /**
+     * This method is a generated getter for the output of a CV_dilate.
+     * @return Mat output from CV_dilate.
+     */
+    public Mat cvDilateOutput() {
+        return cvDilateOutput;
+    }
+
+    /**
+     * Segment an image based on hue, saturation, and value ranges.
+     *
+     * @param input The image on which to perform the HSL threshold.
+     * @param hue The min and max hue
+     * @param sat The min and max saturation
+     * @param val The min and max value
+     * @param output The image in which to store the output.
+     */
+    private void hsvThreshold(Mat input, double[] hue, double[] sat, double[] val,
+                              Mat out) {
+        Imgproc.cvtColor(input, out, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(out, new Scalar(hue[0], sat[0], val[0]),
+                new Scalar(hue[1], sat[1], val[1]), out);
+    }
+
+    /**
+     * An indication of which type of filter to use for a blur.
+     * Choices are BOX, GAUSSIAN, MEDIAN, and BILATERAL
+     */
+    enum BlurType{
+        BOX("Box Blur"), GAUSSIAN("Gaussian Blur"), MEDIAN("Median Filter"),
+        BILATERAL("Bilateral Filter");
+
+        private final String label;
+
+        BlurType(String label) {
+            this.label = label;
+        }
+
+        public static BlurType get(String type) {
+            if (BILATERAL.label.equals(type)) {
+                return BILATERAL;
+            }
+            else if (GAUSSIAN.label.equals(type)) {
+                return GAUSSIAN;
+            }
+            else if (MEDIAN.label.equals(type)) {
+                return MEDIAN;
+            }
+            else {
+                return BOX;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return this.label;
+        }
+    }
+
+    /**
+     * Softens an image using one of several filters.
+     * @param input The image on which to perform the blur.
+     * @param type The blurType to perform.
+     * @param doubleRadius The radius for the blur.
+     * @param output The image in which to store the output.
+     */
+    private void blur(Mat input, BlurType type, double doubleRadius,
+                      Mat output) {
+        int radius = (int)(doubleRadius + 0.5);
+        int kernelSize;
+        switch(type){
+            case BOX:
+                kernelSize = 2 * radius + 1;
+                Imgproc.blur(input, output, new Size(kernelSize, kernelSize));
+                break;
+            case GAUSSIAN:
+                kernelSize = 6 * radius + 1;
+                Imgproc.GaussianBlur(input,output, new Size(kernelSize, kernelSize), radius);
+                break;
+            case MEDIAN:
+                kernelSize = 2 * radius + 1;
+                Imgproc.medianBlur(input, output, kernelSize);
+                break;
+            case BILATERAL:
+                Imgproc.bilateralFilter(input, output, -1, radius, radius);
+                break;
+        }
+    }
+
+    /**
+     * Expands area of lower value in an image.
+     * @param src the Image to erode.
+     * @param kernel the kernel for erosion.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the erosion.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvErode(Mat src, Mat kernel, Point anchor, double iterations,
+                         int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null) {
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+    }
+
+    /**
+     * Expands area of higher value in an image.
+     * @param src the Image to dilate.
+     * @param kernel the kernel for dilation.
+     * @param anchor the center of the kernel.
+     * @param iterations the number of times to perform the dilation.
+     * @param borderType pixel extrapolation method.
+     * @param borderValue value to be used for a constant border.
+     * @param dst Output Image.
+     */
+    private void cvDilate(Mat src, Mat kernel, Point anchor, double iterations,
+                          int borderType, Scalar borderValue, Mat dst) {
+        if (kernel == null) {
+            kernel = new Mat();
+        }
+        if (anchor == null) {
+            anchor = new Point(-1,-1);
+        }
+        if (borderValue == null){
+            borderValue = new Scalar(-1);
+        }
+        Imgproc.dilate(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+    }
+
 }
