bad402979baa994b61274279074e8c21cfe4ecfe
ver09934 <ianvernooy@gmail.com>
2018-10-06 16:43:03 -0400

    Work on detecting cubes in addition to balls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleBlueVision.java
@@ -8,8 +8,10 @@ package org.firstinspires.ftc.teamcode;
 import org.corningrobotics.enderbots.endercv.OpenCVPipeline;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ExampleBlueVision extends OpenCVPipeline {
@@ -22,6 +24,8 @@ public class ExampleBlueVision extends OpenCVPipeline {
     private List<MatOfPoint> whiteContours = new ArrayList<>();
     private List<MatOfPoint> yellowContours = new ArrayList<>();
 
+    private List<MatOfPoint> goodYellowContours = new ArrayList<>();
+
     public synchronized List<MatOfPoint> getContours() {
         return whiteContours; // TODO
     }
@@ -33,6 +37,7 @@ public class ExampleBlueVision extends OpenCVPipeline {
         // Clear contour lists
         yellowContours.clear();
         whiteContours.clear();
+        goodYellowContours.clear();
 
         // Change colorspace from RGBA to HSV
         Imgproc.cvtColor(rgba, hsv, Imgproc.COLOR_RGB2HSV, 3);
@@ -45,15 +50,33 @@ public class ExampleBlueVision extends OpenCVPipeline {
         Imgproc.erode(thresholdedWhite,  thresholdedWhite, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
         Imgproc.dilate(thresholdedWhite,  thresholdedWhite, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
 
-        // Yellow threshold, erode, and dilate
-        Core.inRange(hsv, new Scalar(10, 180, 110), new Scalar(30, 255, 255), thresholdedYellow);
-        Imgproc.erode(thresholdedYellow, thresholdedYellow, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
-        Imgproc.dilate(thresholdedYellow, thresholdedYellow, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
-
         // Modify mask to fill holes
         // Use Imgproc.RETR_EXTERNAL instead of Imgproc.RETR_LIST to find only external contours
         Imgproc.findContours(thresholdedWhite, whiteContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
         Imgproc.fillPoly(thresholdedWhite, whiteContours, new Scalar(255, 255, 255));
+
+        // Yellow threshold, erode, and dilate
+        Core.inRange(hsv, new Scalar(10, 180, 200), new Scalar(35, 255, 255), thresholdedYellow); // Conservative
+        // Core.inRange(hsv, new Scalar(0, 160, 180), new Scalar(40, 255, 255), thresholdedYellow); // Liberal
+        Imgproc.erode(thresholdedYellow, thresholdedYellow, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
+        Imgproc.dilate(thresholdedYellow, thresholdedYellow, new Mat(), new Point(-1, -1), 5, Core.BORDER_CONSTANT);
+
+        // Preliminary block location algorithm
+        Imgproc.findContours(thresholdedYellow, yellowContours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        Collections.sort(yellowContours, (c1, c2) -> Double.compare(Imgproc.contourArea(c1), Imgproc.contourArea(c2)));
+        for (int i = 0; i < Math.min(1, yellowContours.size()); i++) {
+            goodYellowContours.add(yellowContours.get(yellowContours.size() - 1 - i));
+        }
+        Imgproc.drawContours(rgba, goodYellowContours, -1, new Scalar(255, 0, 0), 2, 15);
+        if (goodYellowContours.size() > 0) {
+            for (MatOfPoint cont : goodYellowContours) {
+                Moments moments = Imgproc.moments(cont);
+                int centerX = (int) (moments.get_m10() / moments.get_m00());
+                int centerY = (int) (moments.get_m01() / moments.get_m00());
+                Point center = new Point(centerX, centerY);
+                Imgproc.circle(rgba, center, 3, new Scalar(255, 0, 255), 2);
+            }
+        }
 
         // --- Find Hough Circles ---
 
@@ -83,7 +106,7 @@ public class ExampleBlueVision extends OpenCVPipeline {
                     // Draw circle perimeter
                     Imgproc.circle(rgba, center, radius, new Scalar(0, 255, 0), 2);
                     // Draw small circle to indicate center
-                    Imgproc.circle(rgba, center, 2, new Scalar(0, 0, 255), 2);
+                    Imgproc.circle(rgba, center, 3, new Scalar(0, 0, 255), 2);
                 }
             }
         }
