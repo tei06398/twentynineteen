a6ad204edaf6e7aa4c39d723849be022e8170967
ver09934 <ianvernooy@gmail.com>
2018-11-02 07:41:15 -0400

    Work on a class for a motor that runs to a position like a servo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionMotor.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class PositionMotor {
+
+    // TODO: Use a weighted value for acceleration?
+
+    public DcMotor motor;
+    private double acceleration;
+    private int currentPosition = 0;
+
+    private final double DEFAULT_POSITION = 0;
+    private final double DEFAULT_ACCELERATION = 0;
+
+    public PositionMotor(DcMotor motor) {
+        this(motor, DEFAULT_ACCELERATION, DEFAULT_POSITION);
+    }
+
+    public PositionMotor(DcMotor motor, double acceleration, int currentPosition) {
+        this.motor = motor;
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        resetEncoder();
+        this.acceleration = acceleration;
+        this.currentPosition = currentPosition;
+    }
+
+    private void resetEncoder() {
+        if (motor.getMode() == DcMotor.RunMode.RUN_USING_ENCODER) {
+            motor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    public int getRealPosition() {
+        return motor.getCurrentPosition();
+    }
+
+    public int getTargetPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(int currentPosition) {
+        this.currentPosition = currentPosition;
+        runToPosition();
+    }
+
+    // TODO: Will this stop the runmode until the method returns, or will it run asynchronously?
+    public void runToPosition() {
+        // do something with currentPosition here
+    }
+
+}
