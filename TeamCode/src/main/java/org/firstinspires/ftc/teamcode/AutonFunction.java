e4b68b75641e20e543b8358db20dfae964269420
ver09934 <ianvernooy@gmail.com>
2018-12-09 13:02:28 -0500

    Write basic landing auton with placeholder values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonFunction.java
@@ -0,0 +1,123 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class AutonFunction {
+
+    private Telemetry telemetry;
+
+    private DcMotor armMotor;
+    private DcMotor winchMotor;
+
+    private static final double WINCH_POWER = 0.2;
+
+    // TODO: Get values
+    private static final int WINCH_COAST_POSITION = 2000; // Two wheels hit ground after coasting, robot stops
+    private static final int WINCH_RUN_POSITION = 2500; // All four wheels hit ground
+
+    private DcMotor.RunMode ARM_MOTOR_RUNMODE = DcMotor.RunMode.RUN_USING_ENCODER;
+    private DcMotor.RunMode WINCH_MOTOR_RUNMODE = DcMotor.RunMode.RUN_USING_ENCODER;
+
+    private Servo lockServo;
+
+    private static final double SERVO_LOCK_POSITION = .35;
+    private static final double SERVO_UNLOCK_POSITION = 0.9;
+
+    public AutonFunction(HardwareMap hardwareMap, Telemetry telemetry) {
+        this.armMotor = hardwareMap.dcMotor.get("armMotor");
+        this.winchMotor = hardwareMap.dcMotor.get("winchMotor");
+        this.lockServo = hardwareMap.servo.get("lockServo");
+
+        this.armMotor.setMode(ARM_MOTOR_RUNMODE);
+        this.winchMotor.setMode(WINCH_MOTOR_RUNMODE);
+
+        // Enable servo controller pwm
+        ServoController servoController = hardwareMap.servoController.get("Servo Controller 1");
+        servoController.pwmEnable();
+    }
+
+    // --- Lock Servo ---
+
+    public void lockServo() {
+        lockServo.setPosition(SERVO_LOCK_POSITION);
+    }
+
+    public void unlockServo() {
+        lockServo.setPosition(SERVO_UNLOCK_POSITION);
+    }
+
+    // --- Winch ---
+
+    // NOTE: Direction winch is wound in will be important
+    // TODO: May have to flip comparators if positions are negative
+
+    public boolean winchCoastFinished() {
+        return winchMotor.getCurrentPosition() > WINCH_COAST_POSITION;
+    }
+
+    public boolean winchRunFinished() {
+        return winchMotor.getCurrentPosition() > WINCH_RUN_POSITION;
+    }
+
+    public void runWinch() {
+        winchMotor.setPower(WINCH_POWER);
+    }
+
+    public void stopWinch() {
+        winchMotor.setPower(0);
+    }
+
+    public int getWinchPosition() {
+        return winchMotor.getCurrentPosition();
+    }
+
+    public void setWinchPower(double power) {
+        winchMotor.setPower(power);
+    }
+
+    public void setWinchTarget(int position) {
+        winchMotor.setTargetPosition(position);
+    }
+
+    public void slackWinch() {
+        winchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    public void brakeWinch() {
+        winchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+     // --- Arm ---
+
+    public int getArmPosition() {
+        return winchMotor.getCurrentPosition();
+    }
+
+    public void slackArm() {
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    public void brakeArm() {
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    // --- Util ---
+
+    public void resetAllEncoders() {
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        winchMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(ARM_MOTOR_RUNMODE);
+        winchMotor.setMode(WINCH_MOTOR_RUNMODE);
+    }
+
+    public void writeTelemetry() {
+        telemetry.addData("Lock Servo Position", lockServo.getPosition());
+        telemetry.addData("Winch Position", winchMotor.getCurrentPosition());
+        telemetry.addData("Arm Position", armMotor.getCurrentPosition());
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2Auton.java
@@ -29,45 +29,82 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@TeleOp(name="RR2 Auton", group="Auton OpMode")
+@Autonomous(name = "RR2 Auton")
 public class RR2Auton extends LinearOpMode {
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DriverFunction driverFunction = new DriverFunction(hardwareMap, telemetry);
+    private ElapsedTime runtime;
+    private DriverFunction driverFunction;
+    private DriverFunction.Steering steering;
+    private  AutonFunction autonFunction;
+
+    public static final double LANDING_SPEED_RATIO = 0.3;
+    public static final double LANDER_ESCAPE_SPEED_RATIO = 0.5;
 
     @Override
     public void runOpMode() {
+
+        // --- Init ---
+        runtime = new ElapsedTime();
+        driverFunction = new DriverFunction(hardwareMap, telemetry);
+        steering = driverFunction.getSteering();
+        autonFunction = new AutonFunction(hardwareMap, telemetry);
+
         telemetry.addData("Status", "Initialized");
+        autonFunction.writeTelemetry();
         telemetry.update();
 
+        driverFunction.resetAllEncoders();
+        autonFunction.resetAllEncoders();
+
+        autonFunction.lockServo();
+
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        autonFunction.writeTelemetry();
+        telemetry.update();
+
+        // --- Wait for start ---
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
+        autonFunction.unlockServo();
+
+        // Coast winch until winch position reaches a relatively steady state
+        while (opModeIsActive() && !autonFunction.winchCoastFinished()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            autonFunction.writeTelemetry();
+            telemetry.update();
+        }
+
+        // Run winch to get us all the way down
+        while (opModeIsActive() && !autonFunction.winchRunFinished()) {
+            autonFunction.runWinch();
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            autonFunction.writeTelemetry();
+            telemetry.update();
+        }
+        autonFunction.stopWinch();
+
+        // Run drive motors to insure we are down
+        steering.moveDegrees(270, LANDING_SPEED_RATIO);
+        steering.finishSteering();
+        sleep(1000);
+        steering.stopAllMotors();
+
+        // Move to the side
+        steering.moveDegrees(270, LANDER_ESCAPE_SPEED_RATIO);
+        steering.finishSteering();
+        sleep(2000);
+        steering.stopAllMotors();
+
+        // run until driver presses stop
         while (opModeIsActive()) {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
+            autonFunction.writeTelemetry();
             telemetry.update();
         }
     }
