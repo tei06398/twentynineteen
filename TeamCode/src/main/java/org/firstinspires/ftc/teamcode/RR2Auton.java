0ffcd6719dcda85cab590c0a15236e6315de9bc0
ver09934 <ianvernooy@gmail.com>
2018-12-29 10:14:46 -0500

    Work on ball knocking in auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2Auton.java
@@ -5,59 +5,92 @@ import android.preference.PreferenceManager;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.corningrobotics.enderbots.endercv.CameraViewDisplay;
 
 @Autonomous(name = "RR2 Auton")
 public class RR2Auton extends LinearOpMode {
+
+    private static SharedPreferences sharedPrefs;
+    private static AutonPosition startPos;
 
     private ElapsedTime runtime;
     private DriverFunction driverFunction;
     private DriverFunction.Steering steering;
     private  AutonFunction autonFunction;
 
+    private WhiteYellowDetector colorDetector;
+
+    public static final double MAX_COAST_SECONDS = 6;
+
+    public static final double NORMAL_SPEED_RATIO = 0.3;
+
+    public static final long MOVE_DELAY_MS = 500; // TODO: Make smaller
+    public static final long LONG_DELAY_MS = 550;
+    private static final long RETREAT_MS = 1300;
+
+    private static final int CV_ITERATIONS = 5;
+    private static final long CV_LOOP_DELAY = 200;
+
+    // TODO: Needed?
     public static final double LANDING_SPEED_RATIO = 0.3;
     public static final double LANDER_ESCAPE_SPEED_RATIO = 0.5;
-    public static final double MAX_COAST_SECONDS = 6;
-    private static SharedPreferences sharedPrefs;
-    private static AutonPosition startPos;
 
     @Override
     public void runOpMode() {
 
         // --- Init ---
+
+        // Initial telemetry
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Get shared preferences for starting position (crater/depot)
         sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.hardwareMap.appContext);
         String position = sharedPrefs.getString("auton_start_position", "ERROR");
         if (position.equals("CRATER")) {
             startPos = AutonPosition.CRATER;
-        } else if (position.equals("DEPOT")) {
+        }
+        else if (position.equals("DEPOT")) {
             startPos = AutonPosition.DEPOT;
-        } else {
+        }
+        else {
             startPos = AutonPosition.NULL;
         }
+
+        // Initialize utilities
         runtime = new ElapsedTime();
         driverFunction = new DriverFunction(hardwareMap, telemetry);
         steering = driverFunction.getSteering();
         autonFunction = new AutonFunction(hardwareMap, telemetry);
 
-        telemetry.addData("Status", "Initialized");
-        autonFunction.writeTelemetry();
-        telemetry.update();
+        // Initialize CV
+        colorDetector = new WhiteYellowDetector(telemetry, true);
+        colorDetector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        colorDetector.enable();
 
+        // Reset encoders
         driverFunction.resetAllEncoders();
         autonFunction.resetAllEncoders();
 
+        // Set motors to initial positions/powers
         autonFunction.lockServo();
         autonFunction.zeroPowerArm(); // We don't want to power the arm until we've retracted all the way
 
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        while (!this.isStarted()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+        // waitForStart();
+
+        // --- Start ---
+
+        runtime.reset();
+
+        telemetry.addData("Status", "Started");
         autonFunction.writeTelemetry();
         telemetry.update();
 
-        // --- Wait for start ---
-        waitForStart();
-        runtime.reset();
-
-        //  TODO: Grab an encoder position here, then add an empirical constant to obtain a reliable target position for arm up
-        // TODO: Alternatively, test out the arm reset method in tele-op (ideally, we would have both)
+        // LANDING
 
         autonFunction.unlockServo();
 
@@ -71,110 +104,251 @@ public class RR2Auton extends LinearOpMode {
         // Run winch to get us all the way down
         while (opModeIsActive() && !autonFunction.winchRunFinished()) {
             autonFunction.runWinch();
-
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             autonFunction.writeTelemetry();
             telemetry.update();
         }
         autonFunction.stopWinch();
 
-        // Run drive motors to insure we are down
-        //steering.moveDegrees(270, LANDING_SPEED_RATIO);
-        //steering.finishSteering();
-        //sleep(2000);
+        // Set a relatively slow speed ratio
+        steering.setSpeedRatio(NORMAL_SPEED_RATIO);
+
+        sleep(MOVE_DELAY_MS);
+
+        // Move against lander
+        steering.moveDegrees(270);
+        steering.finishSteering();
+        sleep(1000);
         steering.stopAllMotors();
 
-        // Get the arm to hold its position
-        autonFunction.powerArm();
-        autonFunction.setArmTargetPosition(autonFunction.getArmPosition());
+        sleep(MOVE_DELAY_MS);
 
-        sleep(500);
-
-        // ----- ESCAPE FROM LANDER -----
-
-        //Ram into Lander
-        /*
-        steering.move(270);
+        // Move away slightly
+        steering.moveDegrees(90);
         steering.finishSteering();
-        sleep(1500);
-        */
-        // Move away from Lander
-        steering.move(90);
+        sleep(200);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Strafe out
+        steering.moveDegrees(0);
         steering.finishSteering();
         sleep(350);
-
-        // Strafe out of Lander
-        steering.move(180);
-        steering.finishSteering();
-        sleep(1500); //TODO: Maybe 2000?
-
-        // Get uncaught from nut
-        steering.move(0);
-        steering.finishSteering();
-        sleep(250);
-
-//        steering.stopAllMotors();
-//        sleep(5000); //TODO Remember to Comment this Out!!
-
-        //Centrally align to Lander
-        /*
-        steering.move(315);
-        steering.finishSteering();
-        sleep(500);
         steering.stopAllMotors();
-        */
 
-        // ----- Perform other auton functions -----
+        sleep(MOVE_DELAY_MS);
 
-        /*if (startPos == AutonPosition.DEPOT) {
-            //Forward up to just before Depot, knocking off the center Mineral
-            steering.move(90);
-            steering.finishSteering();
-            sleep(2000);
-            //Turn to be Parallel to Depot
-            steering.turnClockwise(0.5);
-            steering.finishSteering();
-            sleep(500);
-            //Move deeper towards Depot
-            steering.move(90);
-            steering.finishSteering();
-            sleep(500);
-            //Drop Team Marker
-            autonFunction.dropMarker();
-            sleep(100);
-            //Move shallower out of Depot
-            steering.move(270);
-            steering.finishSteering();
-            sleep(500);
-            //Move left along Depot towards Wall
-            steering.move(0);
-            steering.finishSteering();
-            sleep(1500);
-            //Move backwards towards Crater
-            steering.move(270);
-            steering.finishSteering();
-            sleep(4000);
-        } else if (startPos == AutonPosition.CRATER) {
-            //Drive forward, knocking off the center Mineral and parking in the Crater
-            steering.move(90);
-            steering.finishSteering();
-            sleep(1000);
-            steering.stopAllMotors();
-            steering.finishSteering();
-        } else {
-            telemetry.addData("HURR", "DURR");
+        // Re-align with lander
+        steering.moveDegrees(270);
+        steering.finishSteering();
+        sleep(400);
+        steering.stopAllMotors();
+
+        sleep(LONG_DELAY_MS);
+
+        // Negative means arm above flat, positive means arm below flat
+        if (autonFunction.getArmPosition() < 0) {
+            autonFunction.powerArm();
+            autonFunction.armDown();
         }
-        // TODO: Replace all the driving stuff here with AutonDriving, if that seems like the right decision
 
-        // run until driver presses stop
+        sleep(LONG_DELAY_MS);
+
+        // Strafe to align center of lander
+        steering.moveDegrees(180);
+        steering.finishSteering();
+        sleep(350);
+        steering.stopAllMotors();
+
+        // COLOR DETECTION
+
+        int[] blockPositions = new int[5];
+        for (int i = 0; i < blockPositions.length; i++) {
+            blockPositions[i] = colorDetector.getPosition();
+            sleep(200);
+        }
+        int[] counts = new int[3];
+        for (int blockPosition : blockPositions) {
+            if (blockPosition >= 0 && blockPosition <= 2) {
+                counts[blockPosition]++;
+            }
+        }
+        MineralPosition mineralPosition;
+        if (counts[0] > counts[1] && counts[0] > counts[2]) {
+            mineralPosition = MineralPosition.LEFT;
+        }
+        else if (counts[1] > counts[0] && counts[1] > counts[2]) {
+            mineralPosition = MineralPosition.CENTER;
+        }
+        else {
+            mineralPosition = MineralPosition.RIGHT;
+        }
+
+        // BALL KNOCKING
+
+        if (startPos == AutonPosition.CRATER) {
+            if (mineralPosition == MineralPosition.LEFT) {
+                knockLeftCrater();
+            }
+            else if (mineralPosition == MineralPosition.CENTER) {
+                knockCenterCrater();
+            }
+            else {
+                knockRightCrater();
+            }
+            // TODO
+        }
+        else if (startPos == AutonPosition.DEPOT) {
+            if (mineralPosition == MineralPosition.LEFT) {
+                knockLeftDepot();
+            }
+            else if (mineralPosition == MineralPosition.CENTER) {
+                knockCenterDepot();
+            }
+            else {
+                knockRightDepot();
+            }
+            // TODO
+        }
+        else {
+            telemetry.addLine("Error: no start position specified");
+            telemetry.update();
+        }
+
+        // run until opmode ends
         while (opModeIsActive()) {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            autonFunction.writeTelemetry();
             telemetry.update();
-        }*/
+        }
+        colorDetector.disable();
+    }
+
+    // --- Crater Side Knocking Methods ---
+
+    public void knockLeftCrater() {
+
+        // Knock
+
+        steering.move(90);
+        steering.finishSteering();
+        sleep(1500);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        steering.move(180);
+        steering.finishSteering();
+        sleep(1700);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        steering.move(90);
+        steering.finishSteering();
+        sleep(1300);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Retreat
+
+        steering.move(270);
+        steering.finishSteering();
+        sleep(RETREAT_MS);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Common right
+
+        steering.move(0);
+        steering.finishSteering();
+        sleep(3300); // 3000
+        steering.stopAllMotors();
+    }
+
+    public void knockCenterCrater() {
+
+        // Knock
+
+        steering.move(90);
+        steering.finishSteering();
+        sleep(2800);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Retreat
+
+        steering.move(270);
+        steering.finishSteering();
+        sleep(RETREAT_MS);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Common right
+
+        steering.move(0);
+        steering.finishSteering();
+        sleep(1800); // 1500
+        steering.stopAllMotors();
+    }
+
+    public void knockRightCrater() {
+        steering.move(90);
+        steering.finishSteering();
+        sleep(1500);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        steering.move(0);
+        steering.finishSteering();
+        sleep(1500);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        steering.move(90);
+        steering.finishSteering();
+        sleep(1300);
+        steering.stopAllMotors();
+
+        sleep(MOVE_DELAY_MS);
+
+        // Retreat
+
+        steering.move(270);
+        steering.finishSteering();
+        sleep(RETREAT_MS);
+        steering.stopAllMotors();
+
+        // Common right already attained
+    }
+
+    // --- Depot Side Knocking Methods ---
+
+    public void knockLeftDepot() {
+    }
+
+    public void knockCenterDepot() {
+    }
+
+    public void knockRightDepot() {
+    }
+
+    // Convert a delay from the intended speed ratio of 0.3 for a different speed ratio by multiplying
+    public long convertDelay(long originalDelay) {
+        return (long) (originalDelay * (NORMAL_SPEED_RATIO / steering.getSpeedRatio()));
     }
 
     private enum AutonPosition {
         CRATER, DEPOT, NULL
+    }
+
+    private enum MineralPosition {
+        LEFT, CENTER, RIGHT
     }
 }
