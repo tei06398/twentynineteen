05260ed51b38e8d330a3ddbe91227f90c045d5bc
ver09934 <ianvernooy@gmail.com>
2019-01-19 12:31:49 -0500

    Add init test auton with only telem and runtime

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitTestAuton.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Init Test Auton")
+public class InitTestAuton extends LinearOpMode {
+
+    private ElapsedTime runtime;
+
+    @Override
+    public void runOpMode() {
+
+        // --- Init ---
+
+        // Initial telemetry
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        runtime = new ElapsedTime();
+
+        while (!this.isStarted()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+
+        // --- Start ---
+
+        runtime.reset();
+
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+
+    }
+}
