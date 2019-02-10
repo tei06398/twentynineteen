cd53d1cf179bf10236f42c785c7bd6f11128a534
ver09934 <ianvernooy@gmail.com>
2019-02-10 10:44:01 -0500

    Disable latency test tele-op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LatencyTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LatencyTestTeleOp.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Disabled
 @TeleOp(name="Latency Test Tele-Op", group="TeleOp OpMode")
 public class LatencyTestTeleOp extends OpMode {
 
