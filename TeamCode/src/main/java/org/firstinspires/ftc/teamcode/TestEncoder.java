33f0aaebaea5888068274e8892934411869101de
ver09934 <ianvernooy@gmail.com>
2019-02-10 07:40:50 -0500

    Re-enable test encoder opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestEncoder.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Disabled
 @TeleOp(name="Encoder Tester", group="TeleOp OpMode")
 public class TestEncoder extends OpMode {
 
